fb02784ca335636b50d28fee8a608ffb90c66ac4
Iz <pehisaac@gmail.com>
2019-01-25 22:38:33 -0700

    Giving div a set height to keep consistant when picture loading.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Thaumatrope from './Thaumatrope'
+import './styles.css'
 
 ReactDOM.render(<Thaumatrope />, document.getElementById('root'))
